aff98afec46333dbc486b4398a42863beac3a938
Guillermo Rivas <gmo.rivas@gmail.com>
2026-04-22 18:49:14 -0600

    abril 22

--- Assets/Scripts/ENTREGA3/Nave.cs
+++ Assets/Scripts/ENTREGA3/Nave.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Nave : MonoBehaviour
 {
@@ -9,9 +10,16 @@ public class Nave : MonoBehaviour
     [SerializeField]
     private float _velocidad = 5;
 
+    // creación dinámica de objetos
+    // cuando creamos nuevos objetos en Unity generalmente clonamos un objeto predefinido
+    // necesitamos un original
+    [SerializeField]
+    private GameObject _original;
+
     void Awake()
     {
         inputActions = new InputSystem_Actions();
+        Assert.IsNotNull(_original, "ORIGINAL ES NULO EN NAVE, VERIFICAR.");
     }
 
     void OnEnable()
@@ -41,6 +49,11 @@ public class Nave : MonoBehaviour
         if(inputActions.Player.Jump.triggered)
         {
             print("PEW PEW!");
+            Instantiate(
+                _original,
+                transform.position,
+                transform.rotation
+            );
         }
     }
 }
--- Assets/Scripts/ENTREGA3/Proyectil.cs
+++ Assets/Scripts/ENTREGA3/Proyectil.cs
@@ -9,12 +9,23 @@ public class Proyectil : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // todos los objetos que son creados deben ser destruidos
+        // por cuestión de salud de la memoria
+        // siempre que haya instantiate debe haber destroy
+
+        // estrategias de destrucción:
+        // - tiempo de vida
+        // - distancia
+        // - colisión
+        // - si salió del espacio visible
+
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(_velocidad * Time.deltaTime, 0, 0);    
+        transform.Translate(_velocidad * Time.deltaTime, 0, 0);
+
     }
 }
